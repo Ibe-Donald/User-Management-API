0d3f8ad978297b547880ecd75cf5255610db3990
Chibuike Ibe <chibuikeibe.ic@gmail.com>
2026-03-24 16:39:37 +0100

    Implemented docker and sql configuration

--- src/main/java/com/chibuike/usermanagement/Entity/User.java
+++ src/main/java/com/chibuike/usermanagement/Entity/User.java
@@ -2,10 +2,7 @@ package com.chibuike.usermanagement.Entity;
 
 import com.chibuike.usermanagement.status.gender;
 import com.chibuike.usermanagement.status.role;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.UUID;
@@ -17,13 +14,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID userId;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "gender", nullable = false)
     private gender gender;
 
+    @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "phoneNumber", nullable = false)
     private String phoneNumber;
 
+    @Column(name = "role", nullable = false)
     private role role;
 }
